424d3cc3d1f7d9835efec8c765e1e21ce347f2e6
Terry Marr & Brigham Thompson <student@epicodus.com>
2015-08-03 15:46:55 -0700

    adds div by 3 & 5

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -1,9 +1,16 @@
+require('pry')
+
 class Fixnum
   define_method(:pingpong) do
     results_array = []
     self.times() do |time|
-      results_array.push(time+1)
+      if ( (time+1).%(15).eql?(0))
+        results_array.push("PingPong")
+      else
+        results_array.push(time+1)
+      end
     end
+
     return results_array
   end
 end
--- spec/pingpong_spec.rb
+++ spec/pingpong_spec.rb
@@ -3,6 +3,10 @@ require('pingpong')
 
 describe('Fixnum#pingpong') do
   it("returns array from 1 to the number") do
-    expect((7).pingpong()).to(eq([1, 2, 3, 4, 5, 6, 7]))
+    expect((2).pingpong()).to(eq([1, 2]))
+  end
+
+  it("if divisible by 3 & 5, return Ping Pong") do
+    expect((15).pingpong().at(14)).to(eq("PingPong"))
   end
 end
